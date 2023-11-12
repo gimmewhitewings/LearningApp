b49336ad07d0ee8d5f78596ec28d319d2f8db190
Stanislav Povedenok <stas.egor.povedenok@gmail.com>
2023-11-12 17:38:32 +0300

    some compose deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,14 @@ android {
 
 dependencies {
 
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+
     implementation("com.google.dagger:hilt-android:2.48.1")
     ksp("com.google.dagger:hilt-android-compiler:2.48.1")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
